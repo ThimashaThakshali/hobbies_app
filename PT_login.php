6560a1d8aacf1ecc999a0ae8f5b7475119253dbc
Thimasha Thakshali <95086384+ThimashaThakshali@users.noreply.github.com>
2023-04-25 21:56:47 +0530

    Update PT_login.php
    Modify PT_login.php

--- PT_login.php
+++ PT_login.php
@@ -2,16 +2,20 @@
 session_start();
 
 	$pagename="Login Page"; 
-	echo "<title>".$pagename."</title>"; 
+	
+    echo "<head>";
+        echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css'>";
+    echo "</head>";
+    
 	echo "<body>";
 	
         //display the name of the web page
-		echo "<h4>".$pagename."</h4>"; 
+		echo "<h2>".$pagename."</h2>"; 
 		
 		//create a form
 		echo"<form action = PT_login_process.php method = post >";
-			echo"<table  style='border: 0px' id = 'logindetails'>";
-		echo"<tr>";
+			echo"<table class=\"table\" style='border: 0px' >";
+		        echo"<tr>";
 					echo"<td style='border: 0px'>E-mail: </td>";
 					echo"<td style='border: 0px'><input type = text name = 'l_email' size=35></td>";
 				echo"</tr>";
@@ -22,8 +26,8 @@ session_start();
 				echo"</tr>";
 				
 				echo"<tr>";
-					echo"<td style='border: 0px'><input type = reset name='submitbtn' value='Clear Form' id='submitbtn'></td>";
-					echo"<td style='border: 0px'><input type = submit name='submitbtn' value='Log In' id='submitbtn'></td>";
+					echo"<td style='border: 0px'><input type = reset name='submitbtn' value='Clear Form' class =\"btn btn-primary\"></td>";
+					echo"<td style='border: 0px'><input type = submit name='submitbtn' value='Log In' class =\"btn btn-primary\"></td>";
 				echo"</tr>";
 				
 			echo"</table>";	
